4e1c32f6bdd5839edebfe1e524e84f91efabb734
wavymtbv <165719562+wavymtbv@users.noreply.github.com>
2024-04-25 13:39:05 -0700

    Add files via upload

---
+++ week1/LiterstoGallonsConverter.java
@@ -0,0 +1,18 @@
+public class LiterstoGallonsConverter {
+
+    public static void main (String[]args) {
+
+        int liters, gallons;
+
+        counter = 0;
+
+        for(liters = 3.79; gallons <= 1; count ++) {
+            gallons = liters /3.79;
+
+            System.out.println(liters);
+            System.out.println(gallons);
+
+        } // end for loop
+        System.out.println(We are done!!!);
+    }
+}
